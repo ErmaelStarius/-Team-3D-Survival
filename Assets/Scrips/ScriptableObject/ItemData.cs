40d3bdf489493a941f00ff41c52c246c7fe13bc0
unm7925 <unm7925@gmail.com>
2024-06-07 21:12:13 +0900

    >> Reset
    >> Reset

--- Assets/Scrips/ScriptableObject/ItemData.cs
+++ Assets/Scrips/ScriptableObject/ItemData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 //아이템의 용도
@@ -14,7 +12,8 @@ public enum ItemType
 public enum ConsumableType
 {
     Hunger,
-    Health
+    Health,
+    Thirst
 }
 
 [System.Serializable]
@@ -44,6 +43,10 @@ public class ItemData : ScriptableObject
     //소비 아이템
     [Header("Consumable")]
     public ItemDataConsumable[] consumables;
+
+    //가공(크래프트) 아이템
+    [Header("Craft")]
+    public bool isCraftItem;
 
     [Header("Equip")]
     public GameObject equipPrefab;

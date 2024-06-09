2c5031e7745d69dcda262983ce098a3b83c1ae3c
unm7925 <unm7925@gmail.com>
2024-06-10 05:41:12 +0900

    >>Add Enemy and Fix Scripts

--- Assets/Scrips/NPC.cs
+++ Assets/Scrips/NPC.cs
@@ -47,7 +47,7 @@ public class NPC : MonoBehaviour, IDamagable
     private Animator animator;
     private SkinnedMeshRenderer[] meshRenderers;
 
-
+    public EnemyData enemyData;
 
     private void Awake()
     {
@@ -63,7 +63,7 @@ public class NPC : MonoBehaviour, IDamagable
         SetState(AIState.Wandering);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         playerDistance = Vector3.Distance(transform.position, CharacterManager.Instance.Player.transform.position);
 
@@ -235,7 +235,9 @@ public class NPC : MonoBehaviour, IDamagable
             Instantiate(dropOnDeath[i].dropPrefab, transform.position + Vector3.up * 2, Quaternion.identity);
         }
 
-        SpawnEnemy._instance.OnSpawn = Die;
+        SpawnEnemy spawnEnemy = FindObjectOfType<SpawnEnemy>();
+
+        spawnEnemy.SpawningPool(enemyData);
 
         Destroy(gameObject);
     }
